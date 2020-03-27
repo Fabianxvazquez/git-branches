826cc89423296f21e0d2f4919c4a55acf88d55d2
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-27 11:13:58 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar/>
+      <h1>Helloss</h1>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+          <h3>Navbar here</h3>
+    </div>
+
+  )
+}
+
+export default Navbar
